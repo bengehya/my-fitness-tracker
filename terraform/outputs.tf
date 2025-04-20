fbc84cad76c63778b97db034df778ec2b023a4c4
bengehya <princebengehya@gmail.com>
2025-04-21 02:50:13 +0300

    add rds instance

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,23 @@ output "bucket_name" {
   description = "The name of the S3 bucket"
   value       = aws_s3_bucket.test_bucket.id
 }
+
+output "db_instance_endpoint" {
+  description = "The endpoint of the RDS instance"
+  value       = aws_db_instance.mydb.endpoint
+}
+
+output "db_instance_identifier" {
+  description = "The RDS instance identifier"
+  value       = aws_db_instance.mydb.id
+}
+
+output "db_username" {
+  description = "The master username for the RDS instance"
+  value       = aws_db_instance.mydb.username
+}
+
+output "db_name" {
+  description = "The database name"
+  value       = aws_db_instance.mydb.db_name
+}
---
+++ terraform/rds.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_db_instance" "mydb" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t2.micro"   # Pour Free Tier 100%
+  db_name              = "mydatabase"
+  username             = "admin"
+  password             = "Lejeune200$"
+  skip_final_snapshot  = true
+  publicly_accessible  = false
+  backup_retention_period = 7
+}
